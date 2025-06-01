17c7172a36501ac4eb644954dd78098380fd07b7
Jou_Tiago <ajourneya@gmail.com>
2025-06-01 18:20:22 -0300

    Initial Commit

---
+++ src/main/java/com/example/redalert/RedalertApplication.java
@@ -0,0 +1,13 @@
+package com.example.redalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redalert/RedalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
